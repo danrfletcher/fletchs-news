426acf29c515dc33cdd9dc97a27091f60eeda205
Dan <danrfletcher@outlook.com>
2023-11-20 15:56:24 +0000

    Fixed previous 2 broken commits

--- controllers/article-controllers.js
+++ controllers/article-controllers.js
@@ -3,7 +3,7 @@ const { selectArticle } = require('../models/article-models.js');
 exports.getArticleByID = (req, res, next) => {
     const { article_id } = req.params;
     selectArticle(article_id).then((article) => {
-        res.status(200).send(article)
+        res.status(200).send({article})
     })
     .catch(next);
 };
