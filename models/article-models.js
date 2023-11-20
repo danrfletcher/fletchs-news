782ef2f8764f8f4a72b4060ceaa1e897554b0f7c
Dan <danrfletcher@outlook.com>
2023-11-20 15:38:18 +0000

    Removed console.log for article-models

--- models/article-models.js
+++ models/article-models.js
@@ -4,7 +4,6 @@ const format = require('pg-format');
 exports.selectArticle = (article_id) => {
     return db.query(format(`SELECT * FROM articles WHERE article_id = %L;`, [article_id]))
     .then((article) => {
-        console.log(article.rows[0])
         return article.rows.length ? article.rows[0] : Promise.reject({status: 404, msg: "article not found"});
     });
 };
