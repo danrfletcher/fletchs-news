172610f803165231eb02f93e7b138af171370a04
Dan <danrfletcher@outlook.com>
2023-11-23 17:08:15 +0000

    Completes refactor to use express routers

---
+++ routes/article-router.js
@@ -0,0 +1,13 @@
+const articlesRouter = require('express').Router();
+
+const { getArticles, getArticleByID, getCommentsByArticleID, changeVotesByArticleID, postCommentByArticleID } = require('../controllers/article-controllers.js');
+articlesRouter.route('/:article_id')
+    .get(getArticleByID)
+    .patch(changeVotesByArticleID);
+articlesRouter.route('/:article_id/comments')
+    .post(postCommentByArticleID)
+    .get(getCommentsByArticleID);
+articlesRouter.get('/', getArticles);
+
+module.exports = articlesRouter;
+
