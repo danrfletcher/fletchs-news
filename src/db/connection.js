d2a87af21c3da6834267125d00de5d53a29c3e39
Dan <danrfletcher@outlook.com>
2023-12-14 23:40:32 +0000

    Adds environment variable for signing jsonwebtokens

--- src/db/connection.js
+++ src/db/connection.js
@@ -2,7 +2,7 @@ const { Pool } = require('pg');
 const ENV = process.env.NODE_ENV || 'development';
 
 require('dotenv').config({
-  path: `${__dirname}/../.env.${ENV}`,
+  path: `${__dirname}/../../.env.${ENV}`,
 });
 
 const config = {};
@@ -14,6 +14,10 @@ if (ENV === 'production') {
 
 if (!process.env.PGDATABASE && !process.env.DATABASE_URL) {
   throw new Error('PGDATABASE or DATABASE_URL not set');
+}
+
+if (!process.env.ACCESS_TOKEN_SECRET) {
+  throw new Error('ACCESS_TOKEN_SECRET not set');
 }
 
 module.exports = new Pool(config);
