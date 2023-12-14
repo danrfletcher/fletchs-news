fdc2663f525a3d4b0caedf7bb0352527a7d38942
Dan <danrfletcher@outlook.com>
2023-12-14 12:40:23 +0000

    Updates user data for testing & development with example comment & article upvotes & downvotes

--- src/db/data/development-data/users.js
+++ src/db/data/development-data/users.js
@@ -4,41 +4,65 @@ module.exports = [
     name: 'Tom Tickle',
     avatar_url:
       'https://vignette.wikia.nocookie.net/mrmen/images/d/d6/Mr-Tickle-9a.png/revision/latest?cb=20180127221953',
-    password: "password"
+    password: 'password',
+    article_upvotes: [2, 4, 7, 10, 15, 18, 21, 24, 27, 30, 33, 36], 
+    article_downvotes: [3, 5, 6, 8, 9, 11, 12, 13, 14, 16, 17, 19, 20, 22, 23, 25, 26, 28, 29, 31, 32, 34, 35], 
+    comment_upvotes: [2, 6, 10, 14, 18, 22, 26, 30, 34], 
+    comment_downvotes: [1, 3, 4, 5, 7, 8, 9, 11, 12, 13, 15, 16, 17, 19, 20, 21, 23, 24, 25, 27, 28, 29, 31, 32, 33, 35, 36, 37],
   },
   {
     username: 'grumpy19',
     name: 'Paul Grump',
     avatar_url:
       'https://vignette.wikia.nocookie.net/mrmen/images/7/78/Mr-Grumpy-3A.PNG/revision/latest?cb=20170707233013',
-    password: "password"
+    password: 'password',
+    article_upvotes: [5, 8, 12, 16, 20, 24, 28, 32], 
+    article_downvotes: [2, 3, 4, 6, 7, 9, 10, 11, 13, 14, 15, 17, 18, 19, 21, 22, 23, 25, 26, 27, 29, 30, 31, 33, 34, 35, 36], 
+    comment_upvotes: [3, 7, 11, 15, 19, 23, 27, 31], 
+    comment_downvotes: [1, 2, 4, 5, 6, 8, 9, 10, 12, 13, 14, 16, 17, 18, 20, 21, 22, 24, 25, 26, 28, 29, 30, 32, 33, 34, 35, 36, 37],
   },
   {
     username: 'happyamy2016',
     name: 'Amy Happy',
     avatar_url:
       'https://vignette1.wikia.nocookie.net/mrmen/images/7/7f/Mr_Happy.jpg/revision/latest?cb=20140102171729',
-    password: "password"
+    password: 'password',
+    article_upvotes: [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15], 
+    article_downvotes: [16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 31, 32, 33, 34, 35, 36], 
+    comment_upvotes: [1, 2, 3, 4, 5, 6, 7, 8, 9, 10], 
+    comment_downvotes: [11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 31, 32, 33, 34, 35, 36, 37],
   },
   {
     username: 'cooljmessy',
     name: 'Peter Messy',
     avatar_url:
       'https://vignette.wikia.nocookie.net/mrmen/images/1/1a/MR_MESSY_4A.jpg/revision/latest/scale-to-width-down/250?cb=20170730171002',
-    password: "password"
+    password: 'password',
+    article_upvotes: [3, 6, 9, 12, 15, 18, 21, 24, 27, 30, 33, 36], 
+    article_downvotes: [1, 2, 4, 5, 7, 8, 10, 11, 13, 14, 16, 17, 19, 20, 22, 23, 25, 26, 28, 29, 31, 32, 34, 35], 
+    comment_upvotes: [2, 5, 8, 11, 14, 17, 20, 23, 26, 29], 
+    comment_downvotes: [1, 3, 4, 6, 7, 9, 10, 12, 13, 15, 16, 18, 19, 21, 22, 24, 25, 27, 28, 30, 31, 32, 33, 34, 35, 36, 37],
   },
   {
     username: 'weegembump',
     name: 'Gemma Bump',
     avatar_url:
       'https://vignette.wikia.nocookie.net/mrmen/images/7/7e/MrMen-Bump.png/revision/latest?cb=20180123225553',
-    password: "password"
+    password: 'password',
+    article_upvotes: [4, 7, 10, 13, 16, 19, 22, 25, 28, 31, 34], 
+    article_downvotes: [2, 3, 5, 6, 8, 9, 11, 12, 14, 15, 17, 18, 20, 21, 23, 24, 26, 27, 29, 30, 32, 33, 35, 36], 
+    comment_upvotes: [1, 4, 7, 10, 13, 16, 19, 22, 25, 28, 31, 34], 
+    comment_downvotes: [2, 3, 5, 6, 8, 9, 11, 12, 14, 15, 17, 18, 20, 21, 23, 24, 26, 27, 29, 30, 32, 33, 35, 36, 37],
   },
   {
     username: 'jessjelly',
     name: 'Jess Jelly',
     avatar_url:
       'https://vignette.wikia.nocookie.net/mrmen/images/4/4f/MR_JELLY_4A.jpg/revision/latest?cb=20180104121141',
-    password: "password"
-  }
+    password: 'password',
+    article_upvotes: [2, 5, 8, 11, 14, 17, 20, 23, 26, 29, 32, 35], 
+    article_downvotes: [1, 3, 4, 6, 7, 9, 10, 12, 13, 15, 16, 18, 19, 21, 22, 24, 25, 27, 28, 30, 31, 33, 34, 36], 
+    comment_upvotes: [3, 6, 9, 12, 15, 18, 21, 24, 27, 30], 
+    comment_downvotes: [1, 2, 4, 5, 7, 8, 10, 11, 13, 14, 16, 17, 19, 20, 22, 23, 25, 26, 28, 29, 31, 32, 33, 34, 35, 36, 37],
+  },
 ];
