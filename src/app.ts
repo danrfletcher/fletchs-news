50dd56c4bd2e361585b8c27b8cbdbe737ea73cf7
Dan <danrfletcher@outlook.com>
2023-12-17 00:00:31 +0000

    Configures cors to ensure cookies are sent correctly and saved by client

--- src/app.ts
+++ src/app.ts
@@ -5,7 +5,24 @@ import apiRouter from './routes/api-router';
 
 const app: Application = express();
 
-app.use(cors());
+const env = process.env.NODE_ENV
+const corsOptions = {
+    credentials: true,
+    origin: (origin: string, callback: Function) => {
+        if (env === 'test' || env === 'development') {
+            callback(null, true);
+        } else {
+            const allowedOrigins = [process.env?.API_BASE_URL];
+            if (!origin || allowedOrigins.indexOf(origin) !== -1) {
+                callback(null, true);
+            } else {
+                callback(new Error('CORS not allowed'), false);
+            }
+        }
+    }
+};
+
+app.use(cors(corsOptions));
 app.use(cookieParser());
 app.use(express.json());
 
