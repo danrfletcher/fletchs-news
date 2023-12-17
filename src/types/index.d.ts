0377f7d93fcdd25ae41e837819970cbfab154863
Dan <danrfletcher@outlook.com>
2023-12-17 13:07:52 +0000

    removes types file

--- src/types/index.d.ts
+++
@@ -1,8 +0,0 @@
-import { JwtPayload } from "jsonwebtoken";
-
-// Allow authenticated user to be placed on request
-declare module 'express-serve-static-core' {
-    interface Request {
-        user?: JwtPayload | String;
-    }
-}
